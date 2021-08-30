5d013d2fae3578ea0895fa4ac830830eb9b09931
Amenallah Hsoumi <amenallah.hsoumi@check24.de>
2021-08-30 12:56:01 +0200

    Display social media icons

--- components/About/About.vue
+++ components/About/About.vue
@@ -1,6 +1,16 @@
 <template>
   <section class="lg:mt-32 mt-24 flex justify-center">
-    <div class="flex flex-col items-center text-center border-gray-300 border-solid lg:border-b pb-4">
+    <div
+      class="
+        flex
+        flex-col
+        items-center
+        text-center
+        border-gray-300
+        border-solid
+        lg:border-b pb-4
+      "
+    >
       <avatar
         :src="require('@/assets/images/amenallah_hsoumi_face.png')"
         alt="Amenallah Hsoumi"
@@ -22,17 +32,39 @@
           Check24
         </a>
       </h2>
+
+      <div class="flex mt-4">
+        <a
+          href="https://github.com/LazyFatArrow"
+          aria-label="github profile"
+          target="_blank"
+        >
+          <github-icon class="w-5 h-5" />
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/amenallah-hsoumi/"
+          aria-label="linkedin profile"
+          target="_blank"
+        >
+          <linkedin-icon class="w-5 h-5 ml-4 text-blue-700 fill-current" />
+        </a>
+      </div>
     </div>
   </section>
 </template>
 
 <script lang="ts">
   import Vue from 'vue'
+  import LinkedinIcon from '../icons/LinkedinIcon.vue'
   import Avatar from '@/components/ui-components/Avatar/Avatar.vue'
+  import GithubIcon from '@/components/icons/GithubIcon.vue'
 
   export default Vue.extend({
     components: {
       Avatar,
+      GithubIcon,
+      LinkedinIcon,
     },
   })
 </script>
