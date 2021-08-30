99591e8d1086c19a82d3c7ce67a97563c0c3683d
Amenallah Hsoumi <amenallah.hsoumi@check24.de>
2021-08-30 12:18:16 +0200

    Add contact details

---
+++ components/Contact/Contact.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="lg:mt-32 mt-24 flex justify-center">
+    <div class="flex flex-col pb-4">
+      <custom-button
+        type="link"
+        link="mailto:amenallah.hsoumi@gmail.com"
+      >
+        Get in touch
+      </custom-button>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import CustomButton from '@/components/ui-components/CustomButton/CustomButton.vue'
+
+  export default Vue.extend({
+    components: {
+      CustomButton,
+    },
+  })
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
     <about />
+
+    <contact />
   </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue'
+  import Contact from '@/components/Contact/Contact.vue'
   import About from '@/components/About/About.vue'
 
   export default Vue.extend({
     components: {
       About,
+      Contact,
     },
   })
 </script>
