1cd49a470d68312e08c75254bcae7fee96b5c931
Amenallah Hsoumi <amenallah.hsoumi@check24.de>
2021-08-31 21:29:57 +0200

    Add footer

---
+++ components/MyFooter/MyFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="lg:mt-12 mt-16 mx-auto lg:w-96 w-72 pb-8">
+    <p class="text-gray-500">
+      &#169; Copyright 2021 Amenallah Hsoumi
+    </p>
+  </footer>
+</template>
