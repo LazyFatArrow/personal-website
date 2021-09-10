ca82bc5421eab0c005fe3ef7c9f9d34f774b8ecf
Amenallah Hsoumi <amenallah.hsoumi@check24.de>
2021-09-10 11:42:46 +0200

    Center text

--- components/MyFooter/MyFooter.vue
+++ components/MyFooter/MyFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="lg:mt-12 mt-16 mx-auto lg:w-96 w-72 pb-8">
-    <p class="text-gray-500">
+    <p class="text-gray-500 text-center">
       &#169; Copyright 2021 Amenallah Hsoumi
     </p>
   </footer>
