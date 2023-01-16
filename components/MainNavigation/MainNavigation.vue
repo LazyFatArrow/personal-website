9900e2393f98314f9b6a00c3a84a6f25fdb9f889
Amenallah Hsoumi <amenallah.hsoumi@gmail.com>
2023-01-16 13:29:02 +0100

    Update MainNavigation.vue
    Update resume link

--- components/MainNavigation/MainNavigation.vue
+++ components/MainNavigation/MainNavigation.vue
@@ -59,7 +59,7 @@
           },
           {
             name: 'Download Resume',
-            path: 'https://drive.google.com/file/d/1txwdWnI521B2Wt65WBBAjmSAm4TRjgWM/view?usp=share_link',
+            path: 'https://drive.google.com/file/d/1AHxYAl9Zkhljo92J69Bo68-xGw7QAblS/view?usp=sharing',
             external: true,
           },
         ],
