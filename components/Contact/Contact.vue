bbb91bba835ac6e915c84ef79ebfaf343a6e5970
amenallah.hsoumi <amenallah.hsoumi@check24.de>
2023-01-29 13:09:50 +0100

    Update contact section

--- components/Contact/Contact.vue
+++ components/Contact/Contact.vue
@@ -1,13 +1,9 @@
 <template>
   <section class="lg:mt-12 mt-16 mx-auto lg:w-96 w-72">
     <div class="flex flex-col items-center pb-4 border-gray-300">
-      <h2 class="text-lg">
-        wanna discuss something 🤔?
-      </h2>
-
       <div class="mt-4">
         <custom-button link="mailto:amenallah.hsoumi@gmail.com">
-          Then lets get in touch!
+          send Amen a message 😀
         </custom-button>
       </div>
     </div>
